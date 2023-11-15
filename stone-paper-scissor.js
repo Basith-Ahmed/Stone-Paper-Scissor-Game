1b469a8998ff379d407e69b4bc2727ba2e7f2d59
Basith Ahmed N A <basithahmeddd@gmail.com>
2023-11-15 14:44:37 +0530

    git status
    clear

--- stone-paper-scissor.js
+++ stone-paper-scissor.js
@@ -14,3 +14,67 @@ function sps() {
     return "Scissors";
   }
 }
+function onclickRock() {
+    r1 = sps()
+    if (r1 === 'Paper') {
+      score.losses = score.losses + 1;
+      console.log(score);
+      alert(`You picked Rock and Computer picked ${r1}. You lost!`);
+    } else if (r1 === 'Rock') {
+      score.tie = score.tie + 1;
+      console.log(score);
+      alert(`You picked Rock and Computer picked ${r1}. Tie!`);
+    } else if (r1 === 'Scissors') {
+      score.win = score.win + 1;
+      console.log(score);
+      alert(`You picked Rock and Computer picked ${r1}. You won!`);
+    }
+    localStorage.setItem('score', JSON.stringify(score));
+}
+function onclickPaper() {
+    r1 = sps();
+    if (r1 === 'Scissors') {
+      score.losses = score.losses + 1;
+      console.log(score);
+      alert(`You picked Paper and Computer picked ${r1}. You lost!`);
+    } else if (r1 === 'Paper') {
+      score.tie = score.tie + 1;
+      console.log(score);
+      alert(`You picked Paper and Computer picked ${r1}. Tie!`);
+    } else if (r1 === 'Rock') {
+      score.win = score.win + 1;
+      console.log(score);
+      alert(`You picked Paper and Computer picked ${r1}. You won!`);
+    }
+    localStorage.setItem('score', JSON.stringify(score));
+}
+function onclickScissor() {
+    r1 = sps()
+    if (r1 === 'Rock') {
+      score.losses = score.losses + 1;
+      console.log(score);
+      alert(`You picked Scissors and Computer picked ${r1}. You lost!`);
+    } else if (r1 === 'Scissors') {
+      score.tie = score.tie + 1;
+      console.log(score);
+      alert(`You picked Scissors and Computer picked ${r1}. Tie!`);
+    } else if (r1 === 'Paper') {
+      score.win = score.win + 1;
+      console.log(score);
+      alert(`You picked Scissors and Computer picked ${r1}. You won!`);
+    }
+    localStorage.setItem('score', JSON.stringify(score));
+}
+function onclickResetScore() {
+      score.win = 0;
+      score.tie = 0;
+      score.losses = 0;
+      console.clear();
+      console.log('Score was reset.');
+      console.log(score);
+      localStorage.removeItem('score');
+}
+function onclickShowScore() {
+      console.clear();
+      console.log(score);
+}
